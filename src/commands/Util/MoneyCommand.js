0a180a1507e26f3f4a6024b40cda41a17af1b42c
DanielaGC <danielagc676@gmail.com>
2019-12-11 00:26:23 -0300

    .

--- src/commands/Util/MoneyCommand.js
+++ src/commands/Util/MoneyCommand.js
@@ -1,13 +1,13 @@
-const Command = require("../../structures/command");
-const ExchangeApi = require('../../api/exchangeApi');
-const InvalidArgumentError = require('../../api/invalidArgumentError');
+const Command = require("../../structures/command")
+const ExchangeApi = require("../../api/exchangeAPI.js")
+const InvalidArgumentError = require("../../api/invalidArgumentError")
 
 module.exports = class MoneyCommand extends Command {
     constructor(client) {
         super(client, {
-        name: 'money',
-        category: 'util',
-        aliases: ['bufunfa', 'currency', 'convercao'],
+        name: "money",
+        category: "util",
+        aliases: ["bufunfa", "currency", "convercao"],
         UserPermission: [],
         ClientPermission: null,
         OnlyDevs: false,
@@ -16,36 +16,36 @@ module.exports = class MoneyCommand extends Command {
     }
 
     async run({ message, args, server }, t) {
-        let [from, to, ammount = 1] = args;
+        let [from, to, ammount = 1] = args
 
-        const { moneybag } = this.client.emotes;
+        const { moneybag } = this.client.emotes
 
         if (!from || !to || (ammount === null)) {
-            message.chinoReply('error', t('commands:money.invalidOptions', {
+            message.chinoReply("error", t("commands:money.invalidOptions", {
                 prefix: server.prefix,
                 name: this.config.name,
-            }));
+            }))
 
-            return;
+            return
         }
 
         if (isNaN(ammount)) {
-            message.chinoReply('error', t('command:money.invalidValue'))
-            return;
+            message.chinoReply("error", t("command:money.invalidValue"))
+            return
         }
 
-        from = from.toUpperCase();
-        to = to.toUpperCase();
+        from = from.toUpperCase()
+        to = to.toUpperCase()
 
         try {
-            const res = await ExchangeApi.getInstance().getExchange(from.toUpperCase(), to.toUpperCase());
-            const total = (res.rates[to] * parseFloat(ammount)).toFixed(2);
-            const totalFormated = Intl.NumberFormat('pt-BR', { style: 'currency', currency: to }).format(total);
+            const res = await ExchangeApi.getInstance().getExchange(from.toUpperCase(), to.toUpperCase())
+            const total = (res.rates[to] * parseFloat(ammount)).toFixed(2)
+            const totalFormated = Intl.NumberFormat("pt-BR", { style: "currency", currency: to }).format(total)
 
-            message.chinoReply('success', moneybag + ' ' + t('commands:money.success', { from, to, ammount, total: totalFormated }));
+            message.chinoReply("success", moneybag + " " + t("commands:money.success", { from, to, ammount, total: totalFormated }))
         } catch (err) {
             if (err instanceof InvalidArgumentError) {
-                message.chinoReply('error', t('commands:money.invalidArgument', { arg: err.message }));
+                message.chinoReply("error", t("commands:money.invalidArgument", { arg: err.message }))
             }
         }
     }

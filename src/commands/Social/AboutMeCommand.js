c90062f6ace4227a4e20e60cab4189c5e2ba480f
DanielaGC <danielagc676@gmail.com>
2019-12-14 16:10:05 -0300

    Fix AboutMeCommand

--- src/commands/Social/AboutMeCommand.js
+++ src/commands/Social/AboutMeCommand.js
@@ -11,8 +11,9 @@ module.exports = class AboutMeCommand extends Command {
 	async run({message, args, server}, t) {
 		let user = await this.client.database.Users.findById(message.author.id)
 		if (!args[0]) return message.chinoReply("error", t("commands:aboutme.args-null"))
+		if (args[0].length > 1024) return message.chinoReply("error", t("commands:aboutme.limited"))
 
-		user.aboutme = args.join(" ")
+		user.aboutme = args.join(" ").replace(/[`]/g, "")
 		user.save()
 
 		message.chinoReply("success", t("commands:aboutme.success", {aboutme: user.aboutme}))

83420008966a0642074d36574d45d00a58be37ed
DanielaGC <danielagc676@gmail.com>
2020-02-02 14:32:48 -0300

    Fixed YensTopCommand

--- src/commands/economy/YensTopCommand.js
+++ src/commands/economy/YensTopCommand.js
@@ -5,33 +5,34 @@ module.exports = class YensTopCommand extends Command {
 		super(client, {
 			name: "yenstop",
 			aliases: ["topyens", "topyen", "yentop"],
-			category: "economy",
-			debug: true
+			category: "economy"
 		})
 	}
 
 	async run({message, args, server}, t) {
 		let usuario = await this.client.database.Users.find({})
-		let number = 0
-		let users = []
-		let members = await this.client.shardManager
-		usuario.filter(async user => await members.getUsers(user._id)).forEach(async user => {
+        let users = []
+        let members = await this.client.shardManager
+        let filteredUsers = usuario.filter(async user => await members.getUsers(user._id))
+        for (let user of filteredUsers) {
 			let us = await members.getUsers(user._id)
-			users.push({
-				tag: us.tag,
-				yens: user.yens
-			})
-		})
-		users.sort(function (a, b) {
-			return b.yens - a.yens
-		})
-		let desc = users.map(a => `**${++number} -** \`${a.tag}\` - *yens: ${Number(a.yens).toLocaleString()}*`).slice(0, 15)
-		let embed = new MessageEmbed()
-		.setColor(this.client.colors.default)
-		.setTitle(`As ${desc.length} pessoas com mais yens`)
-		.setDescription(desc)
-		.setThumbnail(this.client.user.displayAvatarURL())
+			if (us) {
+				users.push({
+					tag: us.tag,
+					yens: user.yens
+				})
+			}
+		}
+        users.sort(function (a, b) {
+            return b.yens - a.yens
+        })
+        let desc = users.map(({tag, yens}, index) => `**${index + 1} -** \`${tag}\` - *yens: ${Number(yens).toLocaleString()}*`).slice(0, 15)
+        let embed = new MessageEmbed()
+        .setColor(this.client.colors.default)
+        .setTitle(`As ${desc.length} pessoas com mais yens`)
+        .setDescription(desc)
+        .setThumbnail(this.client.user.displayAvatarURL())
 
-		message.channel.send(embed)
-	}
+        message.channel.send(embed)
+    }
 }

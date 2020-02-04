d2f1e23595a1cb60dc2ce9dcc7943155c7dc42f2
DanielaGC <danielagc676@gmail.com>
2020-02-03 21:56:10 -0300

    Adaptando o userinfo pra shards

--- src/commands/util/UserinfoCommand.js
+++ src/commands/util/UserinfoCommand.js
@@ -16,7 +16,7 @@ module.exports = class UserinfoCommand extends Command {
 
 		moment.locale(server.lang)
 
-		let member = message.mentions.users.first() || this.client.users.get(args[0]) || message.author
+		let member = args[0] ? await this.client.shardManager.getUsers(args[0].replace(/[<@!>]/g, "")) : message.author
 		let status = `${member.presence.status}`.replace("dnd", t("commands:userinfo.statusDnd", { emoji: this.client.emotes.dnd })).replace("idle", t("commands:userinfo.statusIdle", { emoji: this.client.emotes.idle })).replace("offline", t("commands:userinfo.statusOffline", { emoji: this.client.emotes.offline })).replace("online", t("commands:userinfo.statusOnline", { emoji: this.client.emotes.online }))
 		let guild = this.client.guilds.filter(g => g.members.get(member.id))
 		let role = message.guild.member(member) ? message.guild.member(member).roles.map(r => r.name).join(", ") : "O usuário não está no servidor"
@@ -38,7 +38,7 @@ module.exports = class UserinfoCommand extends Command {
 			.setColor(color)
 			.setThumbnail(member.displayAvatarURL())
 			.setDescription(t("commands:userinfo.title", { isBot: member.bot ? "<:botTag:579456048142876672>" : "<:Wumpus:579455982053097485>", member: member.tag }), member.displayAvatarURL())
-			.addField(t("commands:userinfo.permissions"), message.guild.member(member) ? `\`${message.guild.member(member).permissions.toArray().join(", ")}\`` : "O usuário não está no servidor")
+			.addField(t("commands:userinfo.permissions"), message.guild.member(member) ? `\`${message.guild.member(member).permissions.toArray().join(", ")}\`` : t("commands:userinfo.no-guild"))
 			.addField(t("commands:userinfo.roles", { roles: roleSize }), `\`${role}\``.replace("@everyone, ", ""), true)
 
 		message.channel.send(embed).then(msg => {

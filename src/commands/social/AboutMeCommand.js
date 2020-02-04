17c094d8f27d000c804f9657c3fe854564af63b9
DanielaGC <danielagc676@gmail.com>
2020-02-04 14:48:15 -0300

    Limitar o AboutMeCommand

--- src/commands/social/AboutMeCommand.js
+++ src/commands/social/AboutMeCommand.js
@@ -11,7 +11,7 @@ module.exports = class AboutMeCommand extends Command {
 	async run({ message, args, server }, t) {
 		let user = await this.client.database.Users.findById(message.author.id)
 		if (!args[0]) return message.chinoReply("error", t("commands:aboutme.args-null"))
-		if (args[0].length > 1024) return message.chinoReply("error", t("commands:aboutme.limited"))
+		if (args[0].length > 120) return message.chinoReply("error", t("commands:aboutme.limited"))
 
 		user.aboutme = args.join(" ").replace(/[`]/g, "")
 		user.save()

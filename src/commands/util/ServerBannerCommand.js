747b2c5b037d2266eacbefbcb203ea0bd0785463
DanielaGC <danielagc676@gmail.com>
2020-02-04 16:33:06 -0300

    Added 2 news commands

---
+++ src/commands/util/ServerBannerCommand.js
@@ -0,0 +1,24 @@
+const Command = require("../../structures/command")
+const { MessageEmbed } = require("discord.js")
+
+module.exports = class ServerBannerCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "serverbanner",
+            aliases: ["guildbanner"],
+            category: "util"
+        })
+    }
+
+    run({ message, args, server }, t) {
+        if (message.guild.premiumTier <= 1 || !message.guild.partnered || !message.guild.verified) return message.chinoReply("error", t("commands:no-premium"))
+        if (!message.guild.banner) return message.chinoReply("error", t("commands:serverbanner.no-banner"))
+
+        const embed = new MessageEmbed()
+            .setColor(this.client.colors.default)
+            .setDescription(t("commands:serverbanner.download", { download: message.guild.bannerURL({ size: 2048 }) }))
+            .setImage(message.guild.bannerURL({ size: 2048 }))
+        
+        message.channel.send(embed)
+    }
+}

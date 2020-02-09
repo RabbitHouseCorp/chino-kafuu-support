7be64015de1c3585f0f674f76323a8d88a3e8f1f
DanielaGC <danielagc676@gmail.com>
2020-02-09 04:24:42 -0300

    Update locales

--- src/commands/games/JanKenPonCommand.js
+++ src/commands/games/JanKenPonCommand.js
@@ -9,10 +9,10 @@ module.exports = class JanKePonCommand extends Command {
     }
 
     async run({ message, args, server }, t) {
-        if (!args[0]) return message.chinoReply("error", "o que você escolhe?")
+        if (!args[0]) return message.chinoReply("error", t("commands:ppt.args-null"))
         let user = await this.client.database.Users.findById(message.author.id)
         let options = ["pedra", "papel", "tesoura"]
-        if (!options.includes(args[0])) return message.chinoReply("error", "você só pode escolher entre `pedra`, `papel` ou `tesoura`")
+        if (!options.includes(args[0])) return message.chinoReply("error", t("commands:ppt.option-not-found"))
         let clientChoice = options[Math.floor(Math.random() * options.length)]
         let me = args[0].toLowerCase()
         let result
@@ -30,15 +30,15 @@ module.exports = class JanKePonCommand extends Command {
         )
         if (userWinOption) {
             emoji = "chino_sad"
-            result = `você escolheu \`${me}\` e eu escolhi \`${clientChoice}\`, parabéns, parece que você ganhou, estou depositando \`${Number(value).toLocaleString()}\` yens na sua conta.`
+            result = t("commands:ppt.you-win", { me: me, clientChoice: clientChoice, value: Number(value).toLocaleString() })
             user.yens += value
             user.save()
         } else if (me === clientChoice) {
             emoji = "chino_thiking"
-            result = "parece que empatamos! Ninguém ganhou ou perdeu."
+            result = t("commands:ppt.tie")
         } else if (userLoserOption) {
             emoji = "chino_kek"
-            result = `você escolheu \`${me}\` e eu escolhi \`${clientChoice}\`, parece que eu ganhei, yay, estou tirando \`${Number(value).toLocaleString()}\` yens da sua conta, ok?`
+            result = t("commands:ppt.you-lose", { me: me, clientChoice: clientChoice, value: Number(value).toLocaleString() })
             user.yens -= value
             user.save()
         }

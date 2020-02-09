54805fe5e69c78475b339d5e528856ba41a6842a
DanielaGC <danielagc676@gmail.com>
2020-02-09 03:22:35 -0300

    Add AddYensCommand

---
+++ src/commands/dev/AddYensCommand.js
@@ -0,0 +1,22 @@
+const Command = require("../../structures/command")
+module.exports = class AddYensCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: "addyens",
+            aliases: [],
+            category: "dev"
+        })
+    }
+    
+    async run({ message, args, server }, t) {
+        if (args[0]) return message.chinoReply("error", "você precisa colocar o ID do usuário.")
+        let member = await this.client.shardManager.getUsers(args[0].replace(/[<@!>]/g, ""))
+        if (!member) return message.chinoReply("error", `eu não encontrei um usuário com o ID \`${args[0]}\`.`)
+        let user = await this.client.database.Users.findById(args[0].replace(/[<@!>]/g, ""))
+        
+        user.yens += args[1]
+        user.save()
+        
+        message.chinoReply("success", `certo, eu adicionei \`${Number(args[1]).toLocaleString()}\` yens na conta do usuário com o ID: \`${args[0]}\``)
+    }
+}

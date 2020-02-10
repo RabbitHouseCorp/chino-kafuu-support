d13035b5b9a96c2906a007286c4bfff82c322de5
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2020-02-10 12:35:27 -0300

    Update AddYensCommand.js

--- src/commands/dev/AddYensCommand.js
+++ src/commands/dev/AddYensCommand.js
@@ -4,12 +4,13 @@ module.exports = class AddYensCommand extends Command {
         super(client, {
             name: "addyens",
             aliases: [],
-            category: "dev"
+            category: "dev",
+            OnlyDevs: true
         })
     }
     
     async run({ message, args, server }, t) {
-        if (args[0]) return message.chinoReply("error", "você precisa colocar o ID do usuário.")
+        if (!args[0]) return message.chinoReply("error", "você precisa colocar o ID do usuário.")
         let member = await this.client.shardManager.getUsers(args[0].replace(/[<@!>]/g, ""))
         if (!member) return message.chinoReply("error", `eu não encontrei um usuário com o ID \`${args[0]}\`.`)
         let user = await this.client.database.Users.findById(args[0].replace(/[<@!>]/g, ""))

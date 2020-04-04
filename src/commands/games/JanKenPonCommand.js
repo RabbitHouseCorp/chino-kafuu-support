ea54fc546739da87ce3517acaa9518982d49abdd
MysteryMS <38635037+MysteryMS@users.noreply.github.com>
2020-04-04 00:21:43 -0300

    Fix

--- src/commands/games/JanKenPonCommand.js
+++ src/commands/games/JanKenPonCommand.js
@@ -12,7 +12,7 @@ module.exports = class JanKePonCommand extends Command {
         if (!args[0]) return message.chinoReply("error", t("commands:ppt.args-null"))
         let user = await this.client.database.Users.findById(message.author.id)
         let options = ["pedra", "papel", "tesoura"]
-        if (!options.includes(args[0])) return message.chinoReply("error", t("commands:ppt.option-not-found"))
+        if (!["pedra", "papel", "tesoura", "rock", "paper", "scissors"].includes(args[0])) return message.chinoReply("error", t("commands:ppt.option-not-found"))
         let clientChoice = options[Math.floor(Math.random() * options.length)]
         let me = args[0].toLowerCase()
         let result

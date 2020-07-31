6dbcf34cb438a62d1a44419dfaba04329fccd02d
BlueSlimee <33792776+BlueSlimee@users.noreply.github.com>
2020-07-30 21:28:18 -0400

    Add client.on("allShardsLaunched")
    Adds a new event called allShardsLaunched, which is emitted when all shards have been successfully launched.

--- shard.js
+++ shard.js
@@ -1,11 +1,21 @@
 const { ShardingManager } = require("discord.js")
+const shardAmount = 2 // How many shards should be spawned
+
 const shards = new ShardingManager("./index.js", {
 	respawn: true,
-	totalShards: 2
+	totalShards: shardAmount
 })
 
+let spawnedShards = 0 // DO NOT CHANGE THIS
 shards.on("shardCreate", (shard) => {
 	console.warn(`Starting shard: ${shard.id}`)
+	
+	spawnedShards += 1
+	if (spawnedShards >= shardAmount) { // If all shards have been launched...
+		shards.shards.forEach(launchedShard => {
+			launchedShard.eval('this.emit("allShardsLaunched")')
+		})
+	}
 })
 
 shards.spawn()

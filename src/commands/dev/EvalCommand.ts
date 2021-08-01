cdcf2d7d3b8460ddcec3804a82d9d55fa4c07d37
DanielaGC <danielagc676@gmail.com>
2021-08-01 00:09:14 -0300

    Adding error's embed

--- src/commands/dev/EvalCommand.ts
+++ src/commands/dev/EvalCommand.ts
@@ -10,12 +10,17 @@ module.exports = class EvalCommand extends CommandListener {
   async run(client: ChinoClient, message: Message, args: string[]) {
     try {
       let evaled = inspect(await eval(args.join(' ')), { depth: 1 })
-      if (evaled.match(new RegExp(client.token))) return message.channel.createMessage(`\`\`\`js\n[REDACTED]\`\`\``)
+      evaled = evaled.replace(new RegExp(`${client.token}`, 'g'), '[REDACTED]')
       if (evaled.length > 1800) evaled = `${evaled.slice(0, 1800)}...`
-      evaled = `\`\`\`js\n${evaled}\`\`\``
+      evaled = `\`\`\`js\n${typeof evaled}\`\`\`\n\`\`\`js\n${evaled}\`\`\``
       await message.channel.createMessage(evaled)
     } catch (error) {
-
+      const embed = new EmbedBuilder()
+      embed.setColor('ERROR')
+      embed.setTitle('Whoops, there is something wrong, check out now.')
+      embed.setDescription(error.stack.slice(0, 1800))
+      
+      message.channel.createMessage(embed.build())
     }
   }
 }

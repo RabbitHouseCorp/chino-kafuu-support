6690440d5d7944398978f24f490cc0d5ab52fd40
DanielaGC <danielagc676@gmail.com>
2021-08-01 00:10:46 -0300

    Migrate the booster's system

---
+++ src/structures/utils/BoosterUtils.ts
@@ -0,0 +1,35 @@
+import { ChinoClient } from '../../ChinoClient'
+import { Guild, Member, TextChannel } from 'eris'
+const { guild_support } = require('../../config')
+import { EmbedBuilder } from '../../structures'
+export class BoosterUtils {
+  static async start(client: ChinoClient, guild: Guild, member: Member) {
+    if (guild.id !== guild_support.id) return
+    if (guild.premiumSubscriptionCount > 40) return
+
+    if (member.premiumSince) {
+      if (member.roles.includes(guild_support.booster.boosterRole) && !member.roles.includes(guild_support.booster.donateRoleID)) return
+
+      const embed = new EmbedBuilder()
+      embed.setColor('DEFAULT')
+      embed.setAuthor(`${member.user.username}#${member.user.discriminator} thank you!`, member.user.avatarURL)
+      embed.setDescription(`${member.user.mention} boosted \`${guild.name}\`. Thank you very much! I added ${Number(guild_support.booster.value).toLocaleString()} yens into your account as a reward for boosting. Enjoy and call your friend to chat with us. Again: thanks.`)
+      embed.setThumbnail(member.user.avatarURL)
+      let user = await client.database.users.getOrCreate(member.user.id)
+      user.yens += Math.round(guild_support.booster.value)
+      user.save()
+      const channel = guild.channels.get(guild_support.booster.channelID) as TextChannel
+      channel.createMessage(embed.build(member.user.mention))
+      try {
+        member.addRole(guild_support.booster.donateRoleID)
+        const dmChannel = await member.user.getDMChannel()
+        dmChannel.createMessage(`Hey ${member.user.mention}, thanks for boosting the \`${guild.name}\`, I added **${Number(guild_support.booster.value).toLocaleString()}** yens into your account.`)
+      } catch {
+        console.error(`The direct message of ${member.user.username}#${member.user.discriminator} are closed.`)
+      }
+    } else {
+      if (member.roles.includes(guild_support.booster.donateRoleID)) return
+      member.removeRole(guild_support.booster.donateRoleID)
+    }
+  }
+}

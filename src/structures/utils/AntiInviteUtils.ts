e0ae3ff18cf545f1096d401ef28be672f3d324f1
DanielaGC <danielagc676@gmail.com>
2021-08-01 00:12:34 -0300

    Adding anti invite system

--- src/structures/utils/AntiInviteUtils.ts
+++ src/structures/utils/AntiInviteUtils.ts
@@ -1,3 +1,8 @@
+import { Invite, Message, TextChannel } from "eris"
+import { ChinoClient } from "../../ChinoClient"
+import { EmbedBuilder } from "./EmbedBuilder"
+const { guild_support } = require('../../config')
+
 export class AntiInviteUtils {
   public static hasInviteOnStatus(activities: object[]) {
     interface Activity {
@@ -5,7 +10,7 @@ export class AntiInviteUtils {
       state: string
     }
 
-    return activities.some((activity: Activity) => activity.type === 4 && this.isInvite(activity.state))
+    return activities.some((activity: Activity) => (activity.type === 4 && this.isInvite(activity.state)) || (activity.type === 0 && this.isInvite(activity.state)))
   }
 
   public static isInvite(text: string) {
@@ -24,5 +29,28 @@ export class AntiInviteUtils {
     }
 
     return (/((?:discord\.gg|discordapp\.com\/invite|discord\.com\/invite|discord\.me|discord\.io))/g).test(text)
+  }
+
+  static async inChannel(client: ChinoClient, message: Message) {
+    if (!this.isInvite(message.content)) return
+    const guild = client.guilds.get(message.guildID)
+    const channel = client.getChannel(guild_support.modLog) as TextChannel
+    if (message.member.roles.includes(guild_support.staffRole)) return
+    const guildInvite = await client.getGuildInvites(message.guildID)
+    const messageInvite = message.content
+      .replace(/(https:\/\/|http:\/\/)/, "")
+      .replace(/(discord\.gg|discord\.com\/invite|discordapp\.com\/invite|discord\.me|discord\.io)/, "")
+      .replace("/", "")
+    if (guildInvite.find((invite: Invite) => invite.code === messageInvite) || (guild.vanityURL !== null && guild.vanityURL === messageInvite)) return
+    const embed = new EmbedBuilder()
+    embed.setColor('MODERATION')
+    embed.setAuthor(`${message.author.username}#${message.author.discriminator} | Warned`, message.author.avatarURL)
+    embed.setThumbnail(client.user.avatarURL)
+    embed.addField('Username', `${message.author.username}#${message.author.discriminator} (\`${message.author.id}\`)`)
+    embed.addField('Who punished', `${client.user.username}#${client.user.discriminator} (\`${client.user.id}\`)`)
+    embed.addField('Reason', '[AUTO MOD] ADVERSITING - Adversiting others Discord server in public chat.')
+    message.delete()
+    message.channel.createMessage(`Hey ${message.author.mention}! Stop right there. You can't adversiting others Discord guild here.`)
+    return channel.createMessage(embed.build())
   }
 }

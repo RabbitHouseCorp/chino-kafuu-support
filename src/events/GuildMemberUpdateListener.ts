6561f040926642825c128de201790215138a2f0c
DanielaGC <danielagc676@gmail.com>
2021-08-01 23:05:48 -0300

    Adding join and leave system

--- src/events/GuildMemberUpdateListener.ts
+++ src/events/GuildMemberUpdateListener.ts
@@ -1,9 +1,11 @@
 import { BoosterUtils } from '../structures'
 import { ChinoClient } from '../ChinoClient'
 import { Guild, Member } from 'eris'
+const { guild_support } = require('../config')
 module.exports = {
   name: 'guildMemberUpdate',
   run: async (client: ChinoClient, guild: Guild, member: Member, oldMember: Member) => {
+    if (guild.id === guild_support.id && !member.pending) member.addRole(guild_support.memberRole)
     BoosterUtils.start(client, guild, member)
   }
 }

99a75c11c85cd65dacf41dd0aa2d9ab997f8e45c
DanielaGC <danielagc676@gmail.com>
2022-02-23 02:11:27 -0300

    Improve anti scam system

--- src/structures/utils/AntiScamUtils.ts
+++ src/structures/utils/AntiScamUtils.ts
@@ -1,6 +1,8 @@
 import { Message, TextableChannel } from 'eris'
 import { EmbedBuilder } from '..'
 import { ChinoClient } from '../../ChinoClient'
+import axios from 'axios'
+import { Logger } from '../utils/Logger'
 interface GuildSupport {
   id: string
   notifyRole: string
@@ -22,21 +24,24 @@ interface GuildSupport {
   }
 }
 
-export function isScam(client: ChinoClient, message: Message<TextableChannel>, guild_support: GuildSupport) {
+export async function isScam(client: ChinoClient, message: Message<TextableChannel>, guild_support: GuildSupport) {
   if (message.member.roles.includes(guild_support.staffRole)) return
-  const regex = (/(?:stmeacomunnitty\.ru|steamcomminuty\.ru|steamcommunytu\.ru|steamcommunytru\.ru|steancomminyitu\.ru|steamcommuntry\.ru|steamcommunytiy\.ru|steancommunytiu\.ru|steancommunnity\.co|steancommunnity\.ru|steancommunnity\.ru|steancommunnity\.co|streammcnmuunity\.ru|streammcomuunity\.ru|steamcommunitlu\.com)/g)
-  if (regex.test(message.content.toLowerCase())) {
-    const guild = client.guilds.get(guild_support.id)
-    const embed = new EmbedBuilder()
-    embed.setColor('MODERATION')
-    embed.setAuthor(`${message.author.username}#${message.author.discriminator} | Banned`, message.author.avatarURL)
-    embed.setThumbnail(client.user.avatarURL)
-    embed.addField('Username', `${message.author.username}#${message.author.discriminator} (\`${message.author.id}\`)`)
-    embed.addField('Who punished', `${client.user.username}#${client.user.discriminator} (\`${client.user.id}\`)`)
-    embed.addField('Reason', '[AUTO MOD] SPAMBOT - Sending a malicious or NSFW URL is not allowed in our guild. Get away from here!')
+  try {
+    const { data: { domains } } = await axios.get('https://raw.githubusercontent.com/nikolaischunk/discord-phishing-links/main/domain-list.json', { responseType: 'json' })
+    if (domains.includes(message.content)) {
+      const guild = client.guilds.get(guild_support.id)
+      const embed = new EmbedBuilder()
+      embed.setColor('MODERATION')
+      embed.setAuthor(`${message.author.username}#${message.author.discriminator} | Banned`, message.author.avatarURL)
+      embed.setThumbnail(client.user.avatarURL)
+      embed.addField('Username', `${message.author.username}#${message.author.discriminator} (\`${message.author.id}\`)`)
+      embed.addField('Who punished', `${client.user.username}#${client.user.discriminator} (\`${client.user.id}\`)`)
+      embed.addField('Reason', '[AUTO MOD] SPAMBOT - Sending a malicious or NSFW URL is not allowed in our guild. Get away from here!')
 
-    message.delete()
-    guild.banMember(message.author.id, 7, '[AUTO MOD] SPAMBOT - Sending a malicious or NSFW URL is not allowed in our guild. Get away from here!')
-    return message.channel.createMessage(embed.build())
+      guild.banMember(message.author.id, 7, '[AUTO MOD] SPAMBOT - Sending a malicious or NSFW URL is not allowed in our guild. Get away from here!')
+      return message.channel.createMessage(embed.build())
+    }
+  } catch {
+    Logger.error('I can\'t access this GitHub\'s raw for an unknown reason...')
   }
 }

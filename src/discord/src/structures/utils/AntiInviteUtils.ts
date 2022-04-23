5f70ab82eab78d9a5918359b0c855583054f1e51
DanielaGC <danielagc676@gmail.com>
2022-04-23 17:52:06 -0300

    feat: Removing anti-invite in the status

--- src/discord/src/structures/utils/AntiInviteUtils.ts
+++ src/discord/src/structures/utils/AntiInviteUtils.ts
@@ -4,15 +4,6 @@ import { EmbedBuilder } from './EmbedBuilder'
 import { Config } from '../../config'
 
 export class AntiInviteUtils {
-  public static hasInviteOnStatus(activities: object[]) {
-    interface Activity {
-      type: number
-      state: string
-    }
-
-    return activities.some((activity: Activity) => (activity.type === 4 && this.isInvite(activity.state)) || (activity.type === 0 && this.isInvite(activity.state)))
-  }
-
   public static isInvite(text: string) {
     if ((/((?:discord\.gg|discordapp\.com\/invite|discord\.com\/invite|discord\.me|discord\.io))/g).test(text)) {
       const textReplace_1: string = text

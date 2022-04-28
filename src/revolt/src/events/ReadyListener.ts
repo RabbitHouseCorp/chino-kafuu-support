c76176dd3e621cf8fdb9bcffe052dc291d961440
DanielaGC <danielagc676@gmail.com>
2022-04-28 16:01:48 -0300

    feat: Edit the bot nickname's if don't have one.

--- src/revolt/src/events/ReadyListener.ts
+++ src/revolt/src/events/ReadyListener.ts
@@ -3,7 +3,14 @@ import { Logger } from '../structures'
 
 export default {
   name: 'ready',
-  run: (client: ChinoClient) => {
+  run: async (client: ChinoClient) => {
+    const me = await client.servers.get(process.env.REVOLT_MAIN_SERVER)?.fetchMember(client.user._id)
+    if (!me.nickname) {
+      me.edit({
+        nickname: 'Chino Kafuu (Support)'
+      })
+    }
+    
     Logger.log('I\'m online now.')
   }
 }

9375934322e25111431aaccf9c45ef48b9046ac0
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2023-03-10 00:11:21 -0300

    Update config.ts

--- src/revolt/src/config.ts
+++ src/revolt/src/config.ts
@@ -1,6 +1,8 @@
 import { ClientOptions } from 'revolt.js'
 import 'dotenv/config'
 export const options: ClientOptions = {
+  apiURL: 'https://api.revolt.chat/',
+  heartbeat: 50,
   cache: false,
   unreads: false,
   debug: false,

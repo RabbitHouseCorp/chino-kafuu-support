2ffdc95c00f87a1a0aa847eb93a51673c2dcf080
DanielaGC <42395960+DanielaGC@users.noreply.github.com>
2023-03-10 00:02:03 -0300

    Update ErrorListener.ts

--- src/revolt/src/events/ErrorListener.ts
+++ src/revolt/src/events/ErrorListener.ts
@@ -4,6 +4,6 @@ import { Logger } from '../structures'
 export default {
   name: 'error',
   run: (client: ChinoClient, error: Error) => {
-    Logger.error(`An unexpected error had happened${error.message}`)
+    Logger.error(`An unexpected error had happened ${error.message}`)
   }
 }

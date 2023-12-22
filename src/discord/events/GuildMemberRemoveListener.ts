4551048498a579e9c346ba42282155e1bfbb0e3b
Nayv <43297329+nayvcake@users.noreply.github.com>
2023-12-22 03:01:44 -0300

    Update GuildMemberRemoveListener.ts

--- src/discord/events/GuildMemberRemoveListener.ts
+++ src/discord/events/GuildMemberRemoveListener.ts
@@ -1,7 +1,7 @@
 import { Guild, Member, TextChannel } from 'eris'
 import { ChinoClient } from '../ChinoClient.platform'
-import { EmbedBuilder } from '../structures'
 import { Config } from '../config'
+import { EmbedBuilder } from '../structures'
 export default {
   name: 'guildMemberRemove',
   run: (client: ChinoClient, guild: Guild, member: Member) => {
@@ -13,9 +13,11 @@ export default {
     embed.setFooter(`Now we have ${guild.memberCount} members`, guild.iconURL)
     embed.addField(`${member.user.username}#${member.user.discriminator} left from the guild`, `See you later **${member.user.username}#${member.user.discriminator}**, we will be waiting for you here in **${guild.name}**, I hope you come back!\n\nWith ${member.user.username} leaving, we now have ${guild.memberCount} people in ${guild.name}.`)
 
-    channel.edit({
-      topic: `[Click to expand] ${guild.memberCount} members | Read the <#${Config.guild_support.infoChannel}> to know what is allowed or not. \n\n**INVITE TO SERVER:** If you want to know about the server invite, here it is: ${guild.vanityURL !== null ? `https://discord.gg/${guild.vanityURL}` : guild.getInvites().then((invites) => `https://discord.gg/${invites[0].code}`)}\n\n**CHINO'S INVITE:** If you want to add it to your server, here is my invite: https://discord.com/oauth2/authorize?client_id=${Config.guild_support.botMain}&scope=bot%20applications.commands&permissions=8560045566`
-    })
-    channel.createMessage(embed.build())
+    Promise.all([
+      channel.edit({
+        topic: `[Click to expand] ${guild.memberCount} members | Read the <#${Config.guild_support.infoChannel}> to know what is allowed or not. \n\n**INVITE TO SERVER:** If you want to know about the server invite, here it is: ${guild.vanityURL !== null ? `https://discord.gg/${guild.vanityURL}` : guild.getInvites().then((invites) => `https://discord.gg/${invites[0].code}`)}\n\n**CHINO'S INVITE:** If you want to add it to your server, here is my invite: https://discord.com/oauth2/authorize?client_id=${Config.guild_support.botMain}&scope=bot%20applications.commands&permissions=8560045566`
+      }),
+      channel.createMessage(embed.build())
+    ])
   }
 }
